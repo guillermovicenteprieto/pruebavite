ba7363e7a68c20740968a85fad8f26f119f15e5c
Guillermo Vicente <84589575+guillermovicenteprieto@users.noreply.github.com>
2021-12-12 18:08:02 -0300

    cambios en ramauno

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import TitleComponente from "./components/TitleComponente";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p> Hello Vite + React!Cambios al repositorio </p>
-        <p> este cambio ésta hecho en la ramauno </p>{" "}
+        <p> este cambio está hecho en la ramauno; time to commit!</p>{" "}
+        <TitleComponente/>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}{" "}
